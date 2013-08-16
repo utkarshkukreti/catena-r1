5677c02cb4218f479f53ff98e91a2d043e05640f
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2013-08-16 17:31:35 +0530

    Add a type alias "Result = Either String State" in Evaluator.

--- src/Catena/Evaluator.hs
+++ src/Catena/Evaluator.hs
@@ -10,28 +10,30 @@ import qualified Data.Map as Map
 data State = State { stack :: [Token] }
              deriving (Eq, Show)
 
+type Result = Either String State
+
 defaultState :: State
 defaultState = State { stack = [] }
 
-evalString :: String -> Either String State
+evalString :: String -> Result
 evalString s = case parse s of
                  Left message -> Left $ "Parse Error: " ++ message ++ "!"
                  Right ts -> eval defaultState ts
 
-eval :: State -> [Token] -> Either String State
+eval :: State -> [Token] -> Result
 eval state [] = Right state
 eval state (x:xs) = case eval1 state x of
                       Left message -> Left message
                       Right newState -> eval newState xs
 
 
-eval1 :: State -> Token -> Either String State
+eval1 :: State -> Token -> Result
 eval1 state (Atom a) = case Map.lookup a builtins of
                          Just f -> f state
                          Nothing -> Left $ "Atom \"" ++ a ++ "\" not found!"
 eval1 state token = Right state { stack = token:(stack state) }
 
-builtins :: Map String (State -> Either String State)
+builtins :: Map String (State -> Result)
 builtins = Map.fromList [
     ("+", iii "+" (+)),
     ("-", iii "-" (-)),
