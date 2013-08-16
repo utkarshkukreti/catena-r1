0d5c78ffc0d95bd6a1483956937e0d433f0a6486
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2013-08-16 11:30:03 +0530

    Add support for parsing simple strings (those without escape sequences).

--- src/Catena/Parser.hs
+++ src/Catena/Parser.hs
@@ -3,17 +3,26 @@ module Catena.Parser (
   parse
 ) where
 
-import Data.Attoparsec.Text (Parser, decimal, parseOnly, signed)
+import Data.Attoparsec.Text (Parser, char, decimal, notChar, parseOnly, signed)
+import Control.Applicative (many, (<|>))
 import Data.String (fromString)
 
 data Token = IntegerT Integer
-  deriving (Eq, Show)
+           | StringT String
+             deriving (Eq, Show)
 
 parse :: String -> Either String Token
 parse = parseOnly root . fromString
 
 root :: Parser Token
-root = integer
+root = integer <|> string
 
 integer :: Parser Token
 integer = fmap IntegerT $ signed decimal
+
+string :: Parser Token
+string = do
+  char '"'
+  s <- many $ notChar '"'
+  char '"'
+  return $ StringT s
--- test/Catena/ParserSpec.hs
+++ test/Catena/ParserSpec.hs
@@ -17,3 +17,9 @@ spec = do
       it "parses negative integers" $ do
         parse "-0" `shouldBe` Right (IntegerT 0)
         parse "-26535" `shouldBe` Right (IntegerT $ -26535)
+
+    context "Strings" $ do
+      it "parses simple strings" $ do
+        parse "\"\"" `shouldBe` Right (StringT "")
+        parse "\"hello\"" `shouldBe` Right (StringT "hello")
+        parse "\" world ! \"" `shouldBe` Right (StringT " world ! ")
