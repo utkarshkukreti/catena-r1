162b8d890ef464cd897d7b3550ef375a7e92f5a4
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2013-08-18 11:59:23 +0530

    Fix error in the REPL.

--- src/Catena/REPL.hs
+++ src/Catena/REPL.hs
@@ -16,7 +16,7 @@ r = putStr "> " >> hFlush stdout >> getLine
 e :: State -> String -> EvalResult
 e state line = case parse line of
   Left err     -> Left $ ParseError err
-  Right tokens -> eval state tokens
+  Right tokens -> eval state { queue = tokens }
 
 p :: State -> EvalResult -> IO State
 p oldState (Left err) = print err >> return oldState
